738710846fb0be34b83a8c5362ab6a69c3cfdb30
imrabti <imrabti@gmail.com>
2015-08-23 10:46:13 +0100

    Spring boot experiment.

--- src/test/java/org/nuvola/rabbit/RabbitmqProtoApplicationTests.java
+++ src/test/java/org/nuvola/rabbit/RabbitmqProtoApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = RabbitmqProtoApplication.class)
 public class RabbitmqProtoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
