6d5206c32c7302bb178dceea6478118a134577a9
imrabti <imrabti@gmail.com>
2015-08-27 07:51:34 +0100

    Added Configuration to use a Spring cloud remote Config server.

--- src/main/java/org/nuvola/rabbit/RabbitmqProtoApplication.java
+++ src/main/java/org/nuvola/rabbit/RabbitmqProtoApplication.java
@@ -2,14 +2,19 @@ package org.nuvola.rabbit;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class RabbitmqProtoApplication implements CommandLineRunner {
     @Autowired
     private RabbitTemplate rabbitTemplate;
+    @Value("${nuvola.url}")
+    private String url;
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqProtoApplication.class, args);
